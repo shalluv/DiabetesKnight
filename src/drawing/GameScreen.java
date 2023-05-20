0eec192c4c14412bd5b68d66c8b364b9c396c337
Fall <surin123.james@gmail.com>
2023-05-20 23:10:03 +0700

    feat: new backgrounds follow player

--- src/drawing/GameScreen.java
+++ src/drawing/GameScreen.java
@@ -12,6 +12,7 @@ public class GameScreen extends Pane {
 	private GameCanvas gameCanvas;
 	private double x;
 	private double y;
+	ImageView[] imageViews;
 
 	public GameScreen() {
 		int mapWidth = Main.mapManager.getMapWidth();
@@ -23,7 +24,7 @@ public class GameScreen extends Pane {
 		setLayoutY(y);
 
 		gameCanvas = new GameCanvas(mapWidth, mapHeight);
-		ImageView[] imageViews = new ImageView[5];
+		imageViews = new ImageView[5];
 		for (int i = 0; i < 5; ++i) {
 			imageViews[i] = new ImageView(
 					Loader.GetSpriteAtlas(Loader.BACKGROUND_ATLAS + String.format("%d.png", i + 1)));
@@ -33,6 +34,10 @@ public class GameScreen extends Pane {
 	}
 
 	public void drawComponent() {
+		for (int i = 0; i < 5; ++i) {
+			imageViews[i].setLayoutX(-x);
+			imageViews[i].setLayoutY(-y);
+		}
 		gameCanvas.drawComponent(x, y, this);
 	}
 
